36361401d1ed32ddabd3329a9541da42bec2539d
henrylong <henrylong719@gmail.com>
2021-03-12 14:35:37 +1030

    display stock info in the front end

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,22 +1,27 @@
 class StocksController < ApplicationController
 
+    def search
 
-  def search
+      if params[:stock].present?
+      
+        @stock = Stock.new_lookup(params[:stock])
 
-    if params[:stock].present?
-    
-      @stock = Stock.new_lookup(params[:stock])
-      if @stock
-        render 'users/my_portfolio'
-      else
-        flash[:alert] = "Please enter a valid symbol to search"
-        redirect_to my_portfolio_path
-
+          if @stock
+            respond_to do |format|
+              format.js { render partial: 'users/result'  }
+            end
+          else 
+            respond_to do |format|
+            # falsh.now avoid the requirement persist one cycle
+            flash.now[:alert] = "Please enter a valid symbol to search"
+            format.js { render partial: 'users/result'  }
+          end
+        end
+      else 
+        respond_to do |format|
+          flash.now[:alert] = "Please enter a valid symbol to search"
+          format.js { render partial: 'users/result'  }
       end
-    else 
-      flash[:alert] = "Please enter a valid symbol to search"
-      redirect_to my_portfolio_path
     end
   end
-
 end
